b4824cc19f3277e202a5d39dda205593bef3f414
giorgio <giorgio.programmer@gmail.com>
2023-09-10 10:47:05 +0200

    reactFormLayout

---
+++ frontend/views/FormLayout/FormLayout.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Row, Col, Card, Form, InputGroup, Button } from 'react-bootstrap';
+
+const FormLayout: React.FC = () => {
+    return (
+        <Row>
+            <Col sm={12}>
+                <Card>
+                    <Card.Header>
+                        <h5>Basic Component</h5>
+                    </Card.Header>
+                    <Card.Body>
+                        <h5>Form controls</h5>
+                        <hr />
+                        <Row>
+                            <Col md={6}>
+                                <Form>
+                                    <Form.Group controlId="exampleInputEmail1">
+                                        <Form.Label>Email address</Form.Label>
+                                        <Form.Control type="email" placeholder="Enter email" />
+                                        <Form.Text className="text-muted">
+                                            We'll never share your email with anyone else.
+                                        </Form.Text>
+                                    </Form.Group>
+                                    {/* ... rest of the form fields ... */}
+                                    <Button variant="primary" type="submit">Submit</Button>
+                                </Form>
+                            </Col>
+                            <Col md={6}>
+                                {/* ... similar transformations for this column ... */}
+                            </Col>
+                        </Row>
+                        {/* ... rest of the card's content ... */}
+                    </Card.Body>
+                </Card>
+                <Card>
+                    <Card.Header>
+                        <h5>Input Group</h5>
+                    </Card.Header>
+                    <Card.Body>
+                        <InputGroup className="mb-3">
+                        <InputGroup.Text>@</InputGroup.Text>
+                            <Form.Control type="text" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" />
+                            <Form.Control type="text" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" />
+                        </InputGroup>
+                        {/* ... rest of the input groups ... */}
+                    </Card.Body>
+                </Card>
+            </Col>
+        </Row>
+    );
+}
+
+export default FormLayout;
