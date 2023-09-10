cfec22c2f982d9dff07c91d49b8ba8c8dab5546e
giorgio <giorgio.programmer@gmail.com>
2023-09-10 10:35:25 +0200

    first commit

---
+++ src/main/java/dev/giorgio/camaleonet/CamaleonetApplication.java
@@ -0,0 +1,13 @@
+package dev.giorgio.camaleonet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamaleonetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamaleonetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/giorgio/camaleonet/CamaleonetApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.giorgio.camaleonet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamaleonetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
