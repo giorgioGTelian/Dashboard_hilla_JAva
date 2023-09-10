e699cebd86ed31bdeff489ce33c0bf263441a956
giorgio <giorgio.programmer@gmail.com>
2023-09-10 11:04:41 +0200

    style

--- frontend/views/MainView.tsx
+++ frontend/views/MainView.tsx
@@ -4,12 +4,15 @@ import { TextField } from "@hilla/react-components/TextField.js";
 import { HelloEndpoint } from "Frontend/generated/endpoints.js";
 import { useState } from "react";
 import FormLayout from './FormLayout/FormLayout';
+import MainMenu from './MainMenu/MainMenu';
+import '../Style/style.css';  // Your custom CSS
 
 export default function MainView() {
   const [name, setName] = useState("");
 
   return (
     <>
+      <MainMenu />
       <FormLayout />
       <TextField
         label="Your name"
